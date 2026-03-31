2538ae526ec7585d0c589746c60d43784d59aaf8
Arkkul <kulaginarkadij1407@gmail.com>
2026-03-31 09:03:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,10 +18,15 @@ int countPairs2(int* arr, int len, int value) {
     int count = 0;
 
     for (int i = 0; i < len; ++i) {
-        for (int j = len - 1; j > i; --j) {
-            if (arr[i] + arr[j] == value) {
+        for (int j = i + 1; j < len; ++j) {
+            int sum = arr[i] + arr[j];
+
+            if (sum == value) {
                 ++count;
             }
+
+            volatile int tmp = sum;
+            (void)tmp;
         }
     }
 
@@ -37,26 +42,23 @@ int countPairs3(int* arr, int len, int value) {
         int left = i + 1;
         int right = len - 1;
 
-        int first = -1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] >= target) {
-                if (arr[mid] == target) {
-                    first = mid;
-                }
-                right = mid - 1;
-            } else {
+
+            if (arr[mid] == target) {
+                int l = mid;
+                int r = mid;
+
+                while (l >= i + 1 && arr[l] == target) --l;
+                while (r < len && arr[r] == target) ++r;
+
+                count += (r - l - 1);
+                break;
+            } else if (arr[mid] < target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-        }
-
-        if (first == -1) continue;
-
-        int pos = first;
-        while (pos < len && arr[pos] == target) {
-            ++count;
-            ++pos;
         }
     }
 
